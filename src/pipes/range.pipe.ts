2736941ff70c0d47ad0972c659c0611e79663327
Jeremy Quick <jrquick@uab.edu>
2019-12-06 15:51:40 -0600

    Better responsive formatting

--- src/pipes/range.pipe.ts
+++ src/pipes/range.pipe.ts
@@ -5,10 +5,13 @@ import {Pipe, PipeTransform} from '@angular/core';
   pure: false
 })
 export class RangePipe implements PipeTransform {
-  transform(items:any[], quantity:number):any {
+  transform(items:any[], iterations:number, start?:number, end?:number):any {
+    start = typeof start === 'number' ? start : 0;
+    end = typeof end === 'number' ? end : iterations + start;
+
     items.length = 0;
 
-    for (let i = 0; i < quantity; i++) {
+    for (let i = start; i < end; i++) {
       items.push(i);
     }
 
