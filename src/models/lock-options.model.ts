8f28bbb4b97d55a8947d0cd254dc7730de2ace8f
Jeremy Quick <jrquick@uab.edu>
2019-12-08 10:14:30 -0600

    Add details

--- src/models/lock-options.model.ts
+++ src/models/lock-options.model.ts
@@ -1,10 +1,9 @@
 export enum LockOptions {
-  DIFFERENT = 'different',
-  IGNORE = 'ignore',
-  LIAR = 'liar',
-  MANUAL = 'manual',
-  ORDER = 'order',
-  RANDOM = 'random',
-  REVERSE = 'reverse',
-  SHUFFLE = 'shuffle',
+  DIFFERENT = 'different', // Lock/Unlock the wrong digit
+  IGNORE = 'ignore', // Ignore clicks
+  LIAR = 'liar', // Show as locked/unlocked when not
+  ORDER = 'order', // The digits must be locked in order, left to right
+  REVERT = 'revert', // Randomly toggle state after locking/unlocking
+  REVERSE = 'reverse', // The digits must be locked in reverse order, right to left
+  SHUFFLE = 'shuffle', // Toggle state after each shuffle
 }
